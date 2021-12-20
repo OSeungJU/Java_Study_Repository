9d8d484f2ad51114486fcffaecc0451e02098ed5
OSeungJU <98tmdwn@naver.com>
2021-12-20 20:17:05 +0900

    깃 허브 업로드 연습

---
+++ Java_code/javaStudy/src/Java_code01/JavaCode01.java
@@ -0,0 +1,10 @@
+package Java_code01;
+
+public class JavaCode01 {
+
+	public static void main(String[] args) {
+
+		// Java 코딩 테스트
+	}
+
+}
---
+++ Java_code/javaStudy/src/Java_test_01/JavaStudy01.java
@@ -0,0 +1,11 @@
+package Java_test_01;
+
+public class JavaStudy01 {
+
+	public static void main(String[] args) {
+
+		System.out.println("Hello Java");
+	}
+
+	
+}
