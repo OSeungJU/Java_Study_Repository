9f5b86ba7b44d16808ffe6328c9de09a5cfd8eea
OSeungJU <98tmdwn@naver.com>
2021-12-24 23:43:27 +0900

    switch문을 활용한 예문

--- Java_code/Java/src/java_study/Java_Study09.java
+++ Java_code/Java/src/java_study/Java_Study09.java
@@ -3,8 +3,27 @@ package java_study;
 public class Java_Study09 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		int month = 10;
+		int day;
+		
+		switch (month) {
+		case 1: case 3: case 5: case 7: case 8: case 10: case 12:
+			day = 31;
+			break;
+		case 2: 
+			day = 28;
+			break;
+		case 4: case 6: case 9: case 11:
+			day = 30;
+			break;
+		default:
+			day = 0;
+			break;
+		
+		}
+			
+		
+		System.out.println(month + "월은" + day + "일 까지 있습니다.");
+			
+		}
 	}
-
-}
