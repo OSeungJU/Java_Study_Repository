858c2b6f491a0c4fd395506d358f4ca5373b8774
OSeungJU <98tmdwn@naver.com>
2021-12-26 13:22:07 +0900

    반복문 무한 반복

---
+++ Java_code/Java/src/java_study/Java_Study13.java
@@ -0,0 +1,24 @@
+package java_study;
+
+public class Java_Study13 {
+//반복문 무한반복
+	public static void main(String[] args) {
+		
+		while(true) {   //while문 무한 반복
+			수행문;
+		}
+		
+		
+		do {            //do while문 무한 반복
+			수행문;
+		}while(true);
+		
+		
+		for(;;) {        //for문 무한 반복
+			수행문
+		}
+		
+		
+	}
+
+}
