de3358b92fcc61c1f01ddc37663fda20459f009f
OSeungJU <98tmdwn@naver.com>
2021-12-28 22:37:38 +0900

    while문 단보다 곱하는 수가 작거나 같은 구구단

--- Java_code/Java/src/java_study/Java_Study19.java
+++ Java_code/Java/src/java_study/Java_Study19.java
@@ -1,19 +1,17 @@
 package java_study;
 
-public class Java_Study18 {
-//반복문 while 짝수 구구단
+public class Java_Study19 {
+//반복문 - while문 단보다 곱하는 수가 작거나 같은경우 까지만 출력하는 구구단
 	public static void main(String[] args) {
-
+	
 		int dan = 2;
 		int times = 1;
 		
 		while(dan <= 9) {
-			if((dan % 2) != 0) {
-				dan++;
-				continue;
-			}
+			
 			times = 1;
 			while(times <= 9) {
+				if(times > dan) break;
 				System.out.println(dan + " X " + times +" = " + dan*times);
 				times++;
 			}
