0363492d389e8ba493427af73f255476db934bca
OSeungJU <98tmdwn@naver.com>
2021-12-29 22:46:30 +0900

    학생 클래스 만들기

---
+++ Java_code/Java/src/java_study/Class_Student.java
@@ -0,0 +1,19 @@
+package java_study;
+
+public class Class_Student {
+// 학생 클래스 만들기
+	public static void main(String[] args) {
+		int studentID;         //학번  
+		String studentName;    //학생 이름
+		int grade;             //학년
+		String address;        //사는 곳
+		// 위의 내용들을 멤버 변수로 정의함
+		
+		//클래스를 정의하는 문법을 간단히 나타내면 다음과 같음
+		//(접근 제어자) class 클래스 이름 {
+		//    맴버 변수;
+		//    메서드
+		// }
+	}
+
+}
