720bec3a805cddce6a47032c12f63e14e3add162
OSeungJU <98tmdwn@naver.com>
2022-01-01 23:26:46 +0900

    매개 변수가 필요없는 함수 예제

---
+++ Java_code/Java/src/java_study/Java_Study22.java
@@ -0,0 +1,15 @@
+package java_study;
+
+public class Java_Study22 {
+
+	int getTenTotal( ) {
+		int i;
+		int total = 0;
+		for(i = 1; i <= 10; i++) {
+			total += 1;
+		}
+		return total;   // 1부터 10까지 더한 값을 반환
+	}
+
+}
+
