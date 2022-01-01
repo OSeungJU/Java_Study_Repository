33b0eeb6c0d4fbf8282b8c9e726bbbc235b157b8
OSeungJU <98tmdwn@naver.com>
2022-01-01 23:24:52 +0900

    함수 정의 예제

---
+++ Java_code/Java/src/java_study/Java_Study21.java
@@ -0,0 +1,14 @@
+package java_study;
+
+public class Java_Study21 {
+
+	
+//      ↓④  ↓①            ↓②
+		int add (int num1, int num2) {
+			int result;
+			result = num1 + num2;
+			return result;
+		}//  ↑③↑
+	}
+//① = 함수 이름,   ② = 매개변수,  ③ = return 예약어,  ④ = 함수 반환형
+
