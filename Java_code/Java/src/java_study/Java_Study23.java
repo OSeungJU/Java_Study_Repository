186badf1ec2fc1f395aedd986eb1282fd38c4a9d
OSeungJU <98tmdwn@naver.com>
2022-01-02 23:29:38 +0900

    return 예약어와 반환형

--- Java_code/Java/src/java_study/Java_Study23.java
+++ Java_code/Java/src/java_study/Java_Study23.java
@@ -1,10 +1,9 @@
 package java_study;
 
 public class Java_Study23 {
-
-	
+// 반환 값이 없는 함수	
 	void printGreeting(String name) {
 		System.out.println(name + "안녕하세요");
-		return;// 변환값 없음
+		return;// 반환값 없음
 	}
 }
