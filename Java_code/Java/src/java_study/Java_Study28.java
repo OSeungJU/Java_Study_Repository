35bd420373c8f4a6f1e19cb6d13dab9b8ff90577
OSeungJU <98tmdwn@naver.com>
2022-01-04 23:48:29 +0900

    클래스에 main( )함수 포함하기

---
+++ Java_code/Java/src/java_study/Java_Study28.java
@@ -0,0 +1,20 @@
+package java_study;
+
+public class Java_Study28 {
+	int sudentID;
+	String studentName;
+	int grade;
+	String address;
+	
+	public String getStudentName( ) {
+		return studentName;
+	}
+	
+	public static void min(String[] args) {
+		Java_Study28 studentAhn = new Java_Study28 ();
+		studentAhn.studentName = "안연수";
+		
+		System.out.println(studentAhn.studentName);
+		System.out.println(studentAhn.getStudentName());
+	}
+}
